5bcd5912c75f383b551f9a17511658e157efbecf
chrisduffy <christopher.s.duffy@gmail.com>
2015-08-12 15:56:38 -0400

    Update ranger.py

--- ranger.py
+++ ranger.py
@@ -7,7 +7,7 @@ Name: ranger.py
 Purpose: To encode commands that execute PowerShell scripts, also provides a wrapper for
 some of the impacket examples and fixes relevant functionality
 
-Copyright (c) 2015, Christopher Duffy & William Butler All rights reserved.
+Copyright (c) 2015, Christopher Duffy All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met: * Redistributions
@@ -21,7 +21,7 @@ products derived from this software without specific prior written permission.
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
 THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL CHRISTOPHER DUFFY & WILLIAM BUTLER BE LIABLE FOR ANY DIRECT, INDIRECT,
+DISCLAIMED. IN NO EVENT SHALL CHRISTOPHER DUFFY BE LIABLE FOR ANY DIRECT, INDIRECT,
 INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
 PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
 INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
